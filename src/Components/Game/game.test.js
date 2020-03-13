374b49457968a9c91635f9d5188417cc196c79d2
Tawakalt <olaniyitaiwo95@yahoo.com>
2020-03-13 20:09:53 +0100

    test that game status is rendered correctly

--- src/Components/Game/game.test.js
+++ src/Components/Game/game.test.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import Game from './game';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 it('renders without crashing', () => {
     shallow(<Game />);
+});
+
+it('renders game status correctly', () => {
+    const wrapper = mount(<Game />);
+    const firstPlayer = 
+      wrapper.find('div.game-info').children().first().text();
+    expect(firstPlayer).toEqual('Next player: X Asc');
+
+    const button = 
+      wrapper.find('button.square').first();
+    button.simulate('click');
+    const secondPlayer = 
+      wrapper.find('div.game-info').children().first().text();
+      expect(secondPlayer).toEqual('Next player: O Asc');
 });
