550de059d887060d73e5bd6c44ad77117e490b8f
Tawakalt <olaniyitaiwo95@yahoo.com>
2020-03-16 11:11:28 +0100

    test that the winner is determined correctly

--- src/Components/Game/game.test.js
+++ src/Components/Game/game.test.js
@@ -19,3 +19,21 @@ it('renders game status correctly', () => {
       wrapper.find('div.game-info').children().first().text();
       expect(secondPlayer).toEqual('Next player: O Asc');
 });
+
+it('determines the winner correctly', () => {
+    const wrapper = mount(<Game />);
+    const turn1 = wrapper.find('button.square').at(0);
+    turn1.simulate('click');
+    const turn2 = wrapper.find('button.square').at(1)
+    turn2.simulate('click');
+    const turn3 = wrapper.find('button.square').at(4)
+    turn3.simulate('click');
+    const turn4 = wrapper.find('button.square').at(5)
+    turn4.simulate('click');
+    const turn5 = wrapper.find('button.square').at(8)
+    turn5.simulate('click');
+
+    const winner = 
+        wrapper.find('div.game-info').children().first().text();
+    expect(winner).toEqual('Winner: X Asc')
+});
