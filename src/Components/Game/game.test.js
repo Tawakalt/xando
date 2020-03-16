a8a0decae902049a15e67bfee0a21bff39887a76
Tawakalt <olaniyitaiwo95@yahoo.com>
2020-03-16 11:12:14 +0100

    test that the game correctly determines a draw

--- src/Components/Game/game.test.js
+++ src/Components/Game/game.test.js
@@ -37,3 +37,29 @@ it('determines the winner correctly', () => {
         wrapper.find('div.game-info').children().first().text();
     expect(winner).toEqual('Winner: X Asc')
 });
+
+it('determines a draw correctly', () => {
+    const wrapper = mount(<Game />);
+    const turn1 = wrapper.find('button.square').at(0);
+    turn1.simulate('click');
+    const turn2 = wrapper.find('button.square').at(1)
+    turn2.simulate('click');
+    const turn3 = wrapper.find('button.square').at(2)
+    turn3.simulate('click');
+    const turn4 = wrapper.find('button.square').at(3)
+    turn4.simulate('click');
+    const turn5 = wrapper.find('button.square').at(5)
+    turn5.simulate('click');
+    const turn6 = wrapper.find('button.square').at(4)
+    turn6.simulate('click');
+    const turn7 = wrapper.find('button.square').at(6)
+    turn7.simulate('click');
+    const turn8 = wrapper.find('button.square').at(8)
+    turn8.simulate('click');
+    const turn9 = wrapper.find('button.square').at(7)
+    turn9.simulate('click');
+
+    const winner = 
+        wrapper.find('div.game-info').children().first().text();
+    expect(winner).toEqual('It\'s a draw! Asc')
+});
